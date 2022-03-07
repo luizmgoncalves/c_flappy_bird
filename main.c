39e2de18e40678fd0443e4ee68b8dd84de19a7b2
Luiz Miguel dos Santos Gonçalves <lm4academy@gmail.com>
2022-03-07 22:51:50 +0000

    animation and mesages added

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+
 #define COLOR_BITS 8
 #define DISPLAY_WIDTH 800
 #define DISPLAY_HEIGHT 400
@@ -14,7 +19,8 @@
 #define PIPES_NUMBER 7
 
 typedef struct {
-	float x, y, r, velx, vely, pontuation;
+	float x, y, r, velx, vely;
+	int jumped, pontuation;
 } Character;
 
 typedef struct {
@@ -55,13 +61,9 @@ void update_pipes(Pipe *pipe_i, Character *player){
 	for (int i=0; i<PIPES_NUMBER; i++){
 		pipe = pipe_i+i;
 		
-		al_draw_filled_rectangle(pipe->x, 0, pipe->x+PIPE_WIDTH, pipe->y1, al_map_rgba(255, 100, 0, 255));
-		al_draw_filled_rectangle(pipe->x, pipe->y1+PIPE_GAP, pipe->x+PIPE_WIDTH, DISPLAY_HEIGHT, al_map_rgba(255, 100, 0, 255));
-		
 		if (player->x > pipe->x && !pipe->passed){
 			pipe->passed = 1;
 			player->pontuation++;
-			printf("%.0f\n", player->pontuation);
 		}
 		
 		if (pipe->x < -PIPE_WIDTH){
@@ -86,9 +88,21 @@ void draw_pipe(Pipe *pipe){
 	al_draw_filled_rectangle(pipe->x, pipe->y1+PIPE_GAP, pipe->x+PIPE_WIDTH, DISPLAY_HEIGHT, al_map_rgba(255, 255, 0, 255));
 }
 
+void update_animation(Character *player){
+	static int index=0;
+	if (!player->jumped) return ;
+	
+	if (index > 10){
+		index=0;
+		player->jumped = 0;
+		return ;
+	}
+	
+	al_draw_filled_circle(player->x, player->y+pow(1.3, index), player->r, al_map_rgba(0, 255, 255, 10));
+	index ++;
+}
+
 void update_player(Character *player){
-	al_draw_filled_circle((int)player->x, (int)player->y, (int)player->r, al_map_rgba(255, 100, 0, 255));
-		
 	player->vely +=0.3;
 		
 	player->y += player->vely;
@@ -99,15 +113,17 @@ int main(){
 	al_install_keyboard();
 	al_install_mouse();
 	al_init_primitives_addon();
+	al_init_image_addon();
+	al_init_font_addon();
+	al_init_ttf_addon();
 	
+	ALLEGRO_FONT *font = al_load_ttf_font("ZacharyBinx.ttf", 20, 0);
 	
 	ALLEGRO_DISPLAY *display = al_create_display(DISPLAY_WIDTH, DISPLAY_HEIGHT);
 
 
 	Character player;
-	
 	Pipe pipes[PIPES_NUMBER];
-	
 	int running;
 
 	
@@ -140,80 +156,21 @@ int main(){
 	ALLEGRO_EVENT keyboard_event;
 	ALLEGRO_EVENT display_event;
 	
-	
-	inicio:;
-	
-	player.x=DISPLAY_WIDTH/2; player.y=DISPLAY_HEIGHT/2; player.r=6; player.velx=0;player.vely=0; 
-	player.pontuation = 0;
-	
-	for (int i=0; i<PIPES_NUMBER; i++){
-		generate_pipe_x(&pipes[i], i);
-		generate_pipe_y(&pipes[i]);
-		pipes[i].passed = 0;
-		if (i==PIPES_NUMBER-1) last_pipe = &pipes[i];
-		al_rest(0.0001);
-	}
-	
-	for (running=1;running;){
-		for (;!al_is_event_queue_empty(mouse_event_queue);){
-			al_get_next_event(mouse_event_queue, &mouse_event);
-			
-			if (mouse_event.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN && mouse_event.mouse.button == 1){
-				player.vely = -5;
-			}
-		}
-		
-		for (;!al_is_event_queue_empty(keyb_event_queue);){
-			al_get_next_event(keyb_event_queue, &keyboard_event);
-		
-			if (keyboard_event.type == ALLEGRO_EVENT_KEY_DOWN && keyboard_event.keyboard.keycode == ALLEGRO_KEY_Q){
-				running=0;
-			}
-		}
-		
-		for (;!al_is_event_queue_empty(display_event_queue);){
-			al_get_next_event(display_event_queue, &display_event);
-			if (display_event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
-				return 0;
-		}
-		
-		update_player(&player);
-		update_pipes(&pipes[0], &player);
-		
-		for (int i=0; i<PIPES_NUMBER; i++){
-			draw_pipe(&pipes[i]);
-		}
-		
-		
-		if(colide(&pipes[0], &player) || player.y < 0 || player.y > DISPLAY_HEIGHT){
-			running = 0;
-		}
-		
-		
-		al_draw_filled_circle((int)player.x, (int)player.y, player.r, al_map_rgba(0, 0, 0, 255));
-		al_flip_display();
-		al_rest(0.01);
-	}
-	
-	
 	al_clear_to_color(al_map_rgba(255, 100, 0, 255));
+	al_draw_text(font, al_map_rgba(0, 0, 0, 255), DISPLAY_WIDTH/2, DISPLAY_HEIGHT/2, ALLEGRO_ALIGN_CENTRE, "Aperte o botao direito do mouse para iniciar o jogo.");
+	
 	al_flip_display();
-	printf("A sua pontuacao final eh de %.0f pontos.\n", player.pontuation);
 	
 	for (running=1;running;){
 		for (;!al_is_event_queue_empty(mouse_event_queue);){
 			al_get_next_event(mouse_event_queue, &mouse_event);
 			if (mouse_event.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN && mouse_event.mouse.button == 2){
-				goto inicio;
+				running = 0;
 			}
 		}
 		
 		for (;!al_is_event_queue_empty(keyb_event_queue);){
 			al_get_next_event(keyb_event_queue, &keyboard_event);
-		
-			if (keyboard_event.type == ALLEGRO_EVENT_KEY_DOWN && keyboard_event.keyboard.keycode == ALLEGRO_KEY_R){
-				goto inicio;
-			}
 		}
 		
 		for (;!al_is_event_queue_empty(display_event_queue);){
@@ -221,8 +178,98 @@ int main(){
 			if (display_event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
 				return 0;
 		}
-		al_rest(0.1);
+		al_rest(0.01);
 	}
+
+	
+	
+	main_loop:;
+	
+		player.x=DISPLAY_WIDTH/2; player.y=DISPLAY_HEIGHT/2; player.r=6; player.velx=0;player.vely=0; 
+		player.pontuation = 0;
+		
+		for (int i=0; i<PIPES_NUMBER; i++){
+			generate_pipe_x(&pipes[i], i);
+			generate_pipe_y(&pipes[i]);
+			pipes[i].passed = 0;
+			if (i==PIPES_NUMBER-1) last_pipe = &pipes[i];
+		}
+		
+		for (running=1;running;){
+			for (;!al_is_event_queue_empty(mouse_event_queue);){
+				al_get_next_event(mouse_event_queue, &mouse_event);
+				
+				if (mouse_event.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN && mouse_event.mouse.button == 1){
+					player.vely = -5;
+					player.jumped = 1;
+				}
+			}
+			
+			for (;!al_is_event_queue_empty(keyb_event_queue);){
+				al_get_next_event(keyb_event_queue, &keyboard_event);
+			
+				if (keyboard_event.type == ALLEGRO_EVENT_KEY_DOWN && keyboard_event.keyboard.keycode == ALLEGRO_KEY_Q){
+					running=0;
+				}
+			}
+			
+			for (;!al_is_event_queue_empty(display_event_queue);){
+				al_get_next_event(display_event_queue, &display_event);
+				if (display_event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+					return 0;
+			}
+			
+			al_clear_to_color(al_map_rgba(255, 100, 0, 255));
+			
+			al_draw_textf(font, al_map_rgba(0, 0, 0, 255), DISPLAY_WIDTH/2-20, 5, ALLEGRO_ALIGN_CENTRE, "%d", player.pontuation);
+			
+			update_player(&player);
+			update_pipes(&pipes[0], &player);
+			
+			for (int i=0; i<PIPES_NUMBER; i++){
+				draw_pipe(&pipes[i]);
+			}
+			
+			
+			if(colide(&pipes[0], &player) || player.y < 0 || player.y > DISPLAY_HEIGHT){
+				running = 0;
+			}
+			
+			update_animation(&player);
+			
+			al_draw_filled_circle((int)player.x, (int)player.y, player.r, al_map_rgba(0, 0, 0, 255));
+			al_flip_display();
+			al_rest(0.01);
+		}
+		
+		al_clear_to_color(al_map_rgba(255, 100, 0, 255));
+		al_draw_textf(font, al_map_rgba(0, 0, 0, 255), DISPLAY_WIDTH/2, DISPLAY_HEIGHT/2, ALLEGRO_ALIGN_CENTRE, "A sua pontuacao final eh de %d pontos.", player.pontuation);
+		
+		al_flip_display();
+		
+		for (running=1;running;){
+			for (;!al_is_event_queue_empty(mouse_event_queue);){
+				al_get_next_event(mouse_event_queue, &mouse_event);
+				if (mouse_event.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN && mouse_event.mouse.button == 2){
+					goto main_loop;
+				}
+			}
+			
+			for (;!al_is_event_queue_empty(keyb_event_queue);){
+				al_get_next_event(keyb_event_queue, &keyboard_event);
+			
+				if (keyboard_event.type == ALLEGRO_EVENT_KEY_DOWN && keyboard_event.keyboard.keycode == ALLEGRO_KEY_R){
+					goto main_loop;
+				}
+			}
+			
+			for (;!al_is_event_queue_empty(display_event_queue);){
+				al_get_next_event(display_event_queue, &display_event);
+				if (display_event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+					return 0;
+			}
+			al_rest(0.1);
+		}
 
 	return 0;
 }
